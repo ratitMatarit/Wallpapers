5a5de93a0e6bd5aba265cec8ecef2e0952c86a5a
ArtHuba <ratitmatarit@gmail.com>
2022-11-21 13:19:54 +0300

    add display metrics

---
+++ app/src/main/java/com/lidera/wallpapers/screens/DisplayMetricActivity.kt
@@ -0,0 +1,11 @@
+package com.lidera.wallpapers.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lidera.wallpapers.R
+
+class DisplayMetricActivity : AppCompatActivity(R.layout.activity_display_metric) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
